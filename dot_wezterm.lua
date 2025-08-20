e8e25802ce62474fe7e8d9a2ddfcf963b963fe14
ReyoKatsu <38076269+Honekatsu@users.noreply.github.com>
2025-08-21 00:03:11 +0900

    feat: add wezterm settings

---
+++ dot_wezterm.lua
@@ -0,0 +1,49 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+local config = wezterm.config_builder()
+
+config.automatically_reload_config = true
+
+config.window_background_opacity = 0.7
+
+config.font = wezterm.font_with_fallback {
+  -- ! Windows
+  "MesloLGL Nerd Font"
+}
+
+config.use_ime = true
+config.check_for_updates = false
+config.audible_bell = "Disabled"
+
+config.mouse_bindings = {
+  {
+    event = {Down = {streak = 1, button = 'Right'}},
+    mods = 'NONE',
+    action = act.PasteFrom 'Clipboard',
+  },
+}
+
+config.color_scheme = 'Darktooth(base16)'
+
+-- ! Windows
+config.keys = {
+  -- CTRL+Tで新しいタブを開く
+  {
+    key = 't',
+    mods = 'CTRL',
+    action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+  },
+
+  -- CTRL+Wで現在のペインを閉じる
+  {
+    key = 'w',
+    mods = 'CTRL',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+}
+
+-- ! Windows
+config.default_prog = { 'powershell.exe' }
+
+return config
